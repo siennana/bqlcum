0c0ddad60a52d4271933f1eae1bae45841dc227e
siennana <sienna.kaylenb@gmail.com>
2024-02-19 22:58:16 -0500

    add banner and update styles

--- dist/deductions/leaderboard-stats.js
+++ dist/deductions/leaderboard-stats.js
@@ -1,30 +1,59 @@
 "use strict";
 const leaderboardUrl = 'https://40ae5vnl08.execute-api.eu-central-1.amazonaws.com/default/dailydeductions';
-const issueDate = '1707948000';
+/**
+ * returns latest date which has passed 5pm EST
+ */
+const getLatestIssue = () => {
+    const currentDate = new Date();
+    currentDate.setMilliseconds(0);
+    currentDate.setSeconds(0);
+    currentDate.setMinutes(0);
+    if (currentDate.getHours() < 17) {
+        currentDate.setTime(currentDate.getTime() - (24 * 60 * 60 * 1000));
+    }
+    currentDate.setHours(17);
+    return Math.floor(currentDate.getTime() / 1000);
+};
 const getUrlForIssue = () => {
+    const issueDate = getLatestIssue();
     return `${leaderboardUrl}?issue=${issueDate}`;
 };
+/**
+ * fetches leaderboard data for a given issue
+ */
 async function fetchLeaderboardData() {
     try {
-        console.log('fuck');
         const url = getUrlForIssue();
         const response = await fetch(url);
-        const data = await response.json();
-        const formattedData = data.map((resObj) => {
-            console.log(new Date(resObj['created_at']));
-            return {
-                name: resObj['name'],
-                created_at: new Date(resObj['created_at'])
-            };
-        });
-        const dataElement = document.getElementById('data');
-        if (dataElement) {
-            dataElement.innerText = JSON.stringify(formattedData, null, 2);
-        }
+        return response.json();
     }
-    catch (_a) {
-        console.log('error fetching data');
+    catch (e) {
+        console.log(`error fetching data`);
     }
 }
 ;
-fetchLeaderboardData();
+/*
+ * gets formatted leaderboard data and adds to DOM
+ */
+async function getLeaderboardData() {
+    const rawData = await fetchLeaderboardData();
+    // sort by created_by date
+    const sortedData = rawData.sort((a, b) => {
+        return a.created_at - b.created_at;
+    });
+    const formattedData = sortedData.map((resObj) => {
+        return {
+            name: resObj['name'],
+            created_at: new Date(resObj['created_at'])
+        };
+    });
+    const dataElement = document.getElementById('data');
+    if (dataElement) {
+        formattedData.forEach((obj) => {
+            const s = `${obj.name} completed at ${new Date(obj.created_at).toLocaleTimeString()}`;
+            const newHtml = `<div>${s}</div>`;
+            dataElement.innerHTML += newHtml;
+        });
+    }
+}
+getLeaderboardData();
