8a0d78094292621c850e82f47543e799b66a36c9
siennana <sienna.kaylenb@gmail.com>
2024-04-27 14:55:55 -0400

    add modal

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
+var ddUrl = '';
+var DD_PSWD = 'titties';
+var sendDDPostRequest = function (str) {
+};
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("test");
     var ddPostButton = document.getElementById("ddPost");
+    var pswdModal = document.querySelector("dialog");
+    var ddInput = document.getElementById("ddInput");
+    var pswdButton = document.getElementById("pswdBtn");
+    var pswdInput = document.getElementById("pswdInput");
     ddPostButton.addEventListener("click", function (event) {
-        console.log("test");
+        console.log(ddInput.value);
+        pswdModal.showModal();
+        pswdButton.addEventListener("click", function (event) {
+            console.log(pswdInput.value);
+            if (pswdInput.value === DD_PSWD) {
+                console.log('match!');
+                pswdModal.close();
+            }
+        });
     });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
+const ddUrl = '';
+const DD_PSWD = 'titties';
 
-  console.log("test");
+const sendDDPostRequest = (str: string) => {
+  
+};
+
+document.addEventListener("DOMContentLoaded", function() {
   const ddPostButton = document.getElementById("ddPost");
+  const pswdModal = document.querySelector("dialog");
+  const ddInput = document.getElementById("ddInput") as HTMLInputElement | null;
+  const pswdButton = document.getElementById("pswdBtn");
+  const pswdInput = document.getElementById("pswdInput") as HTMLInputElement | null;
+
   ddPostButton.addEventListener("click", function(event) {
-    console.log("test");
+    console.log(ddInput.value);
+    pswdModal.showModal();
+
+    pswdButton.addEventListener("click", function(event) {
+      console.log(pswdInput.value);
+      if (pswdInput.value === DD_PSWD) {
+        console.log('match!');
+      }
+      pswdModal.close();
+    });
   });
 });
 
